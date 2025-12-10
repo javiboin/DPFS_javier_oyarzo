15c9608628cc018425ac6f531328011a1a490ef2
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-10 15:39:23 -0300

    Update app.js: Se implementa method override para utilizar los verbos HTTP put y delete en formularios

--- routes/products.js
+++ routes/products.js
@@ -7,8 +7,8 @@ router.get('/product-detail/:id', productController.show);
 router.get('/new-product', productController.create);
 router.post('/create', productController.store);
 router.get('/:id', productController.edit);
-router.post('/update/:id', productController.update);
-router.post('/delete-product/:id', productController.destroy);
+router.put('/update/:id', productController.update);
+router.delete('/delete-product/:id', productController.destroy);
 
 router.get('/product-cart', function(req, res, next) {
   res.render('products/productCart', { title: 'Carrito de Compras' });
