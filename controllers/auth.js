0309b822c81ff1fac98634255d8ad62c44915221
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-15 16:17:19 -0300

    Add: bcrypt para encriptar contraseña en el registro de usuarios

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
 const users = require("../data/users");
+const bcryptjs = require('bcryptjs');
+
+const createHash = async (passwordToConvert) => {
+  const newHash = await bcryptjs.hash(passwordToConvert, 8);
+  return newHash;
+};
 
 const authController = {
     login: (req, res, next) => {
         res.render('users/login', { title: 'Inicio de Sesión' });
     },
     access: (req, res, next) => {
+
         res.send('Login');
     },
     logout: (req, res, next) => {
@@ -13,25 +20,26 @@ const authController = {
     signup: (req, res, next) => {
         res.render('users/register', { title: 'Registro' });
     },
-    register: (req, res, next) => {
+    register: async (req, res, next) => {
         const { firstName, lastName, email, password, confirmPassword } = req.body
-        // Validar password
+
         if (password !== '' && confirmPassword !== ''){
             if (password !== confirmPassword){
                 return res.send('Las contraseñas no son iguales')
             }
         } 
 
+        const passwordEncrypted = await createHash(password);
+
         const newUser = {
             id: users.data.length +1,
             firstName: firstName,
             lastName: lastName,
             email: email,
-            password: password,
+            password: passwordEncrypted,
             category: 'Cliente',
             image: req.file.filename 
         };
-
 
         users.data.push(newUser);
         res.redirect('/users/login');
