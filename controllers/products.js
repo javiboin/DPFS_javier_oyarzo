e4775d7e4549cbbb57d1dc7739477883a0113241
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-19 12:43:51 -0300

    Update Controller y View: Detalle de producto

--- controllers/products.js
+++ controllers/products.js
@@ -15,35 +15,43 @@ const productController = {
               ]
             });
             //res.json(products);
-            res.render('index', { title: 'Sound City Music', products })
+            return res.render('index', { title: 'Sound City Music', products })
 
         } catch (error) {
-            res.status(500).json({ error: error.message });
+            return res.status(500).json({ error: error.message });
         }
         
     },
-    show: (req, res, next) => {
-        const id = req.params.id;
-        const product = searchProduct(id);
-        
-        if (!product) {
-            return res.status(404).render('not-found');
+    show: async (req, res, next) => {
+        try {
+            const id = parseInt(req.params.id);
+            const product = await db.Product.findByPk(id, {
+                include: [
+                    { association: 'brand' },
+                    { association: 'subcategory' }
+                ]
+            })
+            
+            //return res.send(showProduct);
+            res.render('products/productDetail', { 
+                title: 'Detalle de productos',
+                id: product.productId,
+                name: product.name,
+                brand: product.brand.name,
+                description: product.description,
+                image: product.image,
+                category: product.category,
+                subcategory: product.subcategory,
+                price: product.price,
+                price_cash: product.priceCash,
+                price_installment_count: product.priceInstallmentCount,
+                price_installment: product.priceInstallment
+            });
+        } catch (error) {
+            return res.status(500).json({ error: error.message})
+            //return res.status(404).render('not-found');
         }
-        
-        res.render('products/productDetail', { 
-            title: 'Detalle de productos',
-            id: product.id,
-            name: product.name,
-            brand: product.brand,
-            description: product.description,
-            image: product.image,
-            category: product.category,
-            subcategory: product.subcategory,
-            price: product.price,
-            price_cash: product.price_cash,
-            price_installment_count: product.price_installment_count,
-            price_installment: product.price_installment
-        });
+       
     },
     create: (req, res, next) => {
         res.render('products/create', { title: 'Alta de Productos' });
