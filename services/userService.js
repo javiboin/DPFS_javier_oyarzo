4e8624420d98d16e84352af90e4eb5097cc4f191
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-21 20:38:24 -0300

    Add: User Services

---
+++ services/userService.js
@@ -0,0 +1,30 @@
+const db = require('../db/models');
+const bcryptjs = require('bcryptjs');
+
+const userService = {
+    createHash: async (passwordToConvert) => {
+        const newHash = await bcryptjs.hash(passwordToConvert, 8);
+        return newHash;
+    },
+
+    compareHash: async (password, hashToConvert) => {
+    const compare = await bcryptjs.compare(password, hashToConvert);
+    return compare;
+    },
+
+    searchUser: async (id) => {
+        const user = await db.User.findByPk(id);
+
+        if (!user){
+            return res.status(404).render('not-found');
+        }
+
+        return user;
+    },
+
+    searchEmailUser: async (email) => {
+        return await db.User.findAll({ where: { email: email } });
+    }
+
+};
+module.exports = userService;
