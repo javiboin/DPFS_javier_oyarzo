af4f1957d24c46af38a272b5c560d2d7643f9f7b
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-22 08:50:09 -0300

    Update Nav: Actualización del sistema de busquedas y navegación

--- controllers/search.js
+++ controllers/search.js
@@ -1,4 +1,4 @@
-const { Product } = require('../db/models');
+const db = require('../db/models');
 const { Op } = require('sequelize'); 
 const searchController = {
     search: async (req, res) => {
@@ -7,12 +7,31 @@ const searchController = {
             if (!q) {
                 return res.redirect('/'); 
             }
-            const products = await Product.findAll({
+            const products = await db.Product.findAll({
                 where: {
                     [Op.or]: [
-                        { name: { [Op.like]: `%${q}%` } }
+                        { name: { [Op.like]: `%${q}%` } },
+                        { '$brand.name$': { [Op.like]: `%${q}%` } },
+                        { '$subcategory.name$': { [Op.like]: `%${q}%` } },
+                        { '$subcategory.category.name$': { [Op.like]: `%${q}%` } }
                     ]
-                }
+                },
+                include: [
+                    { 
+                        model: db.Brand,
+                        as: 'brand',
+                        attributes: ['name']
+                    },
+                    {
+                        model: db.Subcategory,
+                        as: 'subcategory',
+                        include: [{
+                            model: db.Category,
+                            as: 'category',
+                            attributes: ['name']
+                        }]
+                    }
+                ]
             });
 
             res.render('products/search-results', { 
