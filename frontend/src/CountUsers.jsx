39d1f604141ee54e2be3f8347eb11e6f4a724b49
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-31 16:50:11 -0300

    Update: count de users con el hook useFetch en dashboard react

--- frontend/src/CountUsers.jsx
+++ frontend/src/CountUsers.jsx
@@ -1,29 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useFetch } from "./hooks/useFetch";
 
 export const CountUsers = () => {
-    const urlBase = 'http://127.0.0.1:3000/api/users'
+    const url = 'http://127.0.0.1:3000/api/users'
 
-    const [items, setItems] = useState([]);
-
-    const fetchUsers = async () => {
-        try {
-            const res = await fetch(urlBase)
-            const data = await res.json()
-            setItems(data)
-        } catch (error) {
-            console.error('Error:', error)
-        }
-    }
+    const { data, isLoading, error, fetchData } = useFetch()
     
     useEffect(() => {
-        fetchUsers()
+        fetchData(url, 'GET')
     }, [])
 
   
     return (
         <>
             <h2>Total de Usuarios</h2>
-            <p>{items.count} usuarios encontrados</p>
+            { isLoading 
+                ? <h4>Cargando...</h4>
+                : error
+                    ? <h4>Ha ocurrido un error: {error}</h4>
+                    :
+                    <p>{data.count} usuarios encontrados</p>
+            } 
         </>
     )
 }
