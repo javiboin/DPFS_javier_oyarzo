e20f680e0ce7d79c0c5c181db62adbe3ab2a47e2
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-12-31 19:35:03 -0300

    Update: Obtener counts para el dashboard en react

--- frontend/src/CountProductsByCategory.jsx
+++ frontend/src/CountProductsByCategory.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useFetch } from "./hooks/useFetch";
 
-export const CountProducts = () => {
-    const url = 'http://127.0.0.1:3000/api/products'
+export const CountProductsByCategory = () => {
+    const url = 'http://127.0.0.1:3000/api/products/by-category'
 
     const { data, isLoading, error, fetchData } = useFetch()
 
@@ -12,15 +12,13 @@ export const CountProducts = () => {
 
     return (
         <>
-            <h2>Total de Productos</h2>
+            <h2>Total de Categorías</h2>
             { isLoading 
                 ? <h4>Cargando...</h4>
                 : error
                     ? <h4>Ha ocurrido un error: {error}</h4>
                     :
-                    <>
-                        <p>{data.cantidad_de_productos} productos encontrados</p>
-                    </>
+                    <p>{data.cantidad_de_productos_por_categoria?.length || 0} categorías encontradas</p>
                 } 
             
         </>
