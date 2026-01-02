5c12ad18b92cdbc8781e18e0f388736e3a60b0e2
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-01-01 23:37:54 -0300

    Add: footer en dashboard react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,34 +12,47 @@ import { ProductsBySubcategory } from './ProductsBySubcategory'
 function App() {
     return (
         <div className='app'>
-            <h1>Sound City Instrumentos - API Dashboard React</h1>
-            <div className='container'>
-                <CountUsers />
-                <CountProducts />
-                <CountProductsByCategory />
-                <CountProductsBySubcategory />
-                <CountBrands />
-            </div>
+            <header className='header'>
+                <img src="/logo-b.png" alt="Sound City Instrumentos Músicales"></img>
+                <h1>API Sound City Instrumentos</h1>
+            </header>
+            <main>
+                <div className='container'>
+                    <CountUsers />
+                    <CountProducts />
+                    <CountProductsByCategory />
+                    <CountProductsBySubcategory />
+                    <CountBrands />
+                </div>
 
-            <hr />
+                <hr />
 
-            <div className="container">
-                <LastProduct />
-                <LastUser />
-            </div>
+                <div className="container">
+                    <LastProduct />
+                    <LastUser />
+                </div>
 
-            <hr />
+                <hr />
+                
+                <h2>Panel de Categorías y Subcategorías con el total de productos de cada una</h2>
+                <div className="subcategory">
+                    <ProductsBySubcategory />
+                </div> 
+
+                <hr />
+                
+                <GetProducts />
+            </main>
             
-            <h2>Panel de Categorías y Subcategorías con el total de productos de cada una</h2>
-            <div className="subcategory">
-                <ProductsBySubcategory />
-            </div> 
-
-            <hr />
-            
-            <GetProducts />
+            <footer>
+                <div>
+                    <img src="/logo-b.png" alt="Sound City Instrumentos Músicales" />
+                    <h3 className="titulo">API Sound City Instrumentos</h3>
+                </div>
+                <p className="legal">Todos los derechos reservados. © 2025 - Hecho por Javier Oyarzo.<a href="https://github.com/javiboin">@javiboin</a></p>
+            </footer>
         </div>
     )
-    }
+}
 
 export default App
