c42ce7b4fc0dbf407fff4203677e74a31b1aea23
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-02-07 22:32:47 -0300

    Update: Agregar calcular cantidades en productos del carrito de compras

--- backend/public/javascripts/changeQuantity.js
+++ backend/public/javascripts/changeQuantity.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.quantity').forEach(input => {
-    const upButton = input.closest('.product-quantity').querySelector('.up');
-    const downButton = input.closest('.product-quantity').querySelector('.down');
+    const upButton = input.closest('.cart-product-quantity').querySelector('.up');
+    const downButton = input.closest('.cart-product-quantity').querySelector('.down');
     
     upButton.addEventListener('click', () => { input.stepUp(); });
     downButton.addEventListener('click', () => { input.stepDown(); });
