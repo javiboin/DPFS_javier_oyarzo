d498ea6a7ae9c8eed6993a963ab710f6c78efdbe
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-03-03 18:54:07 -0300

    Add: Manejador de rutas. Dashboard será una sección - Preparando terreno para nuevas rutas en la app react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,13 @@
-import './App.css'
-import { CountUsers } from './components/CountUsers'
-import { CountProducts } from './components/CountProducts'
-import { LastProduct } from './components/LastProduct'
-import { LastUser } from './components/LastUser'
-import { CountProductsByCategory } from './components/CountProductsByCategory'
-import { CountProductsBySubcategory } from './components/CountProductsBySubcategory'
-import { CountBrands } from './components/CountBrands'
-import { GetProducts } from './components/GetProducts'
-import { ProductsBySubcategory } from './components/ProductsBySubcategory'
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Dashboard } from "./pages/Dashboard"
 
 function App() {
     return (
-        <div className='app'>
-            <header className='header'>
-                <img src="/logo-b.png" alt="Sound City Instrumentos Músicales"></img>
-                <h1>API Sound City Instrumentos</h1>
-            </header>
-            <main>
-                <div className='container'>
-                    <CountUsers />
-                    <CountProducts />
-                    <CountProductsByCategory />
-                    <CountProductsBySubcategory />
-                    <CountBrands />
-                </div>
-
-                <hr />
-
-                <div className="container">
-                    <LastProduct />
-                    <LastUser />
-                </div>
-
-                <hr />
-                
-                <h2>Panel de Categorías y Subcategorías con el total de productos de cada una</h2>
-                <div className="subcategory">
-                    <ProductsBySubcategory />
-                </div> 
-
-                <hr />
-                
-                <GetProducts />
-            </main>
-            
-            <footer>
-                <div>
-                    <img src="/logo-b.png" alt="Sound City Instrumentos Músicales" />
-                    <h3 className="titulo">API Sound City Instrumentos</h3>
-                </div>
-                <p className="legal">Todos los derechos reservados. © 2025 - Hecho por Javier Oyarzo. <a href="https://github.com/javiboin">@javiboin</a></p>
-            </footer>
-        </div>
+        <Routes>
+            <Route path='/' element={<Dashboard />}></Route>
+            <Route path='/dashboard' element={<Dashboard />}></Route>
+            <Route path='/*' element={<Navigate to='/' />}></Route>
+        </Routes>
     )
 }
 
